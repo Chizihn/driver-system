f0cec38f03227a50defed87b5c5b18a79e539fe0
chizihn <chizihn@gmail.com>
2025-10-19 10:27:28 +0100

    again

--- backend/dist/src/config/database.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-// Simple Prisma client export used by repositories
-exports.prisma = new client_1.PrismaClient();
-exports.default = exports.prisma;
